9aa9c33b0a63119d648c00f1fb3f0035b7b7d873
BradyDouthit <brady.douthit99@gmail.com>
2025-03-03 15:28:28 +0000

    add colorizer

--- init.lua
+++ init.lua
@@ -243,7 +243,7 @@ vim.opt.rtp:prepend(lazypath)
 require('lazy').setup({
   -- NOTE: Plugins can be added with a link (or for a github repo: 'owner/repo' link).
   'tpope/vim-sleuth', -- Detect tabstop and shiftwidth automatically
-
+  'chrisbra/Colorizer', -- Add visual color to hex codes, etc. NOTE: Run :h Colorizer for more info
   -- NOTE: Plugins can also be added by using a table,
   -- with the first argument being the link and the following
   -- keys can be used to configure plugin behavior/loading/etc.
